73930e0818fc697a042deab0581d5de140824bfb
ronan <carreroronan@gmail.com>
2019-02-08 22:31:47 -0300

    ejercicios hasta el 8 terminado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,25 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+  	
+	var importe; 
+	var aumento;
+	var sueldoMasAumento;
+
+
+
+	importe = sueldo.value;
+	aumento = resultado.value;
+
+	importe = parseInt(importe);
+	aumento = parseInt(aumento);
+
+	sueldoMasAumento = importe * 10/100;
+
+	total = sueldoMasAumento + importe;
+
+
+	alert("RESULTADO: " + total);
 
 	
 }
